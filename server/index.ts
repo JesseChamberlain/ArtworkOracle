a8400ac8ae9936fd46ec3f6e1c958d9ddcf17e34
Jesse Chamberlain <chamberlain.jesse@gmail.com>
2025-03-29 13:26:33 -0400

    Configure artsy endpoint (#5)
    * initial connection and random gene response
    
    * package-lock removal
    
    * chained calls to pull random gene and random artist
    
    * reset app.tsx change
    
    * missed a change still
    
    ---------
    
    Co-authored-by: Jesse <Jesse@Kaitlins-MacBook-Air.local>

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,13 @@ import cors from "cors";
 import helmet from "helmet";
 import dotenv from "dotenv";
 import path from "path";
+import { randomInt } from "crypto";
+const traverson = require("traverson");
+const JsonHalAdapter = require("traverson-hal");
+
+// Register the HAL adapter
+traverson.registerMediaType(JsonHalAdapter.mediaType, JsonHalAdapter);
+const artsyApi = traverson.from("https://api.artsy.net/api").jsonHal();
 
 // Load environment variables
 dotenv.config();
@@ -15,6 +22,47 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Interfaces
+
+// Calls a random gene, and then selects a random artist that represents that gene
+async function getRandomArtistByGene() {
+  try {
+    artsyApi
+      .newRequest()
+      .follow("genes")
+      .withRequestOptions({
+        headers: {
+          "X-Xapp-Token": process.env.ARTSY_TEMP_TOKEN,
+          Accept: "application/vnd.artsy-v2+json",
+        },
+      })
+      .withTemplateParameters({ sample: "true" })
+      .getResource(function (error: Error | null, gene: any, traversal: any) {
+        if (gene && typeof gene === "object") {
+          console.log("Gene name: ", gene.name);
+
+          traversal
+            .continue()
+            .follow("artists")
+            .withTemplateParameters({ gene_id: gene.id })
+            .getResource(function (error: Error | null, artists: any) {
+              const artist =
+                artists._embedded.artists[
+                  randomInt(artists._embedded.artists.length - 1)
+                ];
+
+              console.log("Random Artist: ", artist.name);
+            });
+        }
+      });
+  } catch (err) {
+    console.error("Failed to connect to Artsy:", err);
+    process.exit(1);
+  }
+}
+
+getRandomArtistByGene();
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "../../dist")));
