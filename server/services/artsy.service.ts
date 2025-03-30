7e1d1241b4124244f701f12a0ec30355b131b681
Jesse <Jesse@Kaitlins-MacBook-Air.local>
2025-03-30 13:11:30 -0400

    simplified Arsty API call and data

--- server/services/artsy.service.ts
+++ server/services/artsy.service.ts
@@ -20,71 +20,74 @@ class ArtsyService {
     this.api = traverson.from(environment.artsyBaseUrl).jsonHal();
   }
 
-  // Gets a random gene from the Artsy API
-  async getRandomGene(): Promise<GeneResponse> {
-    return new Promise((resolve, reject) => {
-      this.api
-        .newRequest()
-        .follow("genes")
-        .withRequestOptions({
-          headers: {
-            "X-Xapp-Token": environment.artsyToken,
-            Accept: "application/vnd.artsy-v2+json",
-          },
-          qs: { sample: "true" },
-        })
-        .getResource((error: Error | null, gene: GeneResponse) => {
-          if (error) {
-            return reject(error);
-          }
-          if (!gene) {
-            return reject(new Error("Invalid gene response from Artsy API"));
-          }
-          resolve(gene);
-        });
-    });
-  }
-
-  // Gets a list of artists by gene and returns one at random
-  async getRandomArtistByGene(geneId: string): Promise<ArtistResponse> {
-    console.log("Gene ID: ", geneId);
-    return new Promise((resolve, reject) => {
-      this.api
-        .newRequest()
-        .follow("artists")
-        .withRequestOptions({
-          headers: {
-            "X-Xapp-Token": environment.artsyToken,
-            Accept: "application/vnd.artsy-v2+json",
-          },
-          qs: { gene_id: geneId },
-        })
-        .getResource((error: Error | null, response: ArtistsByGeneResponse) => {
-          console.log("ArtistsByGene: ", response);
-          if (error) {
-            return reject(error);
-          }
-          if (!response || !response._embedded.artists.length) {
-            return reject(new Error("No artists found for this gene"));
-          }
-
-          const artists = response._embedded.artists;
-          console.log("Artists: ", artists);
-          const randomIndex = randomInt(0, artists.length - 1);
-          const artist: ArtistResponse = artists[randomIndex];
-          console.log("Artist: ", artist);
-          resolve(artist);
-        });
-    });
-  }
-
-  // Calls the Artsy API to retrieve a random gene and artist
+  // Calls the Artsy API to retrieve a random gene and then artist
   async getRandomGeneAndArtist(): Promise<ArtsyData> {
     try {
-      const gene: GeneResponse = await this.getRandomGene();
-      console.log("Gene: ", gene);
-      const artist: ArtistResponse = await this.getRandomArtistByGene(gene.id);
-      return { gene, artist };
+      return new Promise((resolve, reject) => {
+        this.api
+          .newRequest()
+          .follow("genes")
+          .withRequestOptions({
+            headers: {
+              "X-Xapp-Token": environment.artsyToken,
+              Accept: "application/vnd.artsy-v2+json",
+            },
+            qs: { sample: "true" },
+          })
+          .getResource(
+            (error: Error | null, gene: GeneResponse, traversal: any) => {
+              if (error) {
+                return reject(error);
+              }
+
+              if (!gene) {
+                return reject(
+                  new Error("Invalid gene response from Artsy API"),
+                );
+              }
+
+              // Utilize traversal to make second API call
+              traversal
+                .continue()
+                .follow("artists")
+                .withRequestOptions({
+                  headers: {
+                    "X-Xapp-Token": environment.artsyToken,
+                    Accept: "application/vnd.artsy-v2+json",
+                  },
+                  qs: { gene_id: gene.id },
+                })
+                .getResource(
+                  (error: Error | null, response: ArtistsByGeneResponse) => {
+                    if (error) {
+                      return reject(error);
+                    }
+
+                    if (!response || !response._embedded.artists.length) {
+                      return reject(
+                        new Error("No artists found for this gene"),
+                      );
+                    }
+
+                    // Selects random artist from ArtistsByGeneResponse
+                    const randomIndex = randomInt(
+                      0,
+                      response._embedded.artists.length - 1,
+                    );
+                    const artist: ArtistResponse =
+                      response._embedded.artists[randomIndex];
+
+                    // Return the gene and artist as an ArstyData type
+                    const artsyData: ArtsyData = { gene, artist };
+                    console.log(
+                      `Gene: ${artsyData.gene.name}, Artist: ${artsyData.artist.name}`,
+                    );
+                    resolve(artsyData);
+                  },
+                );
+            },
+          );
+      });
     } catch (error) {
       console.log("Error getting random gene and artist: ", error);
       throw error;
